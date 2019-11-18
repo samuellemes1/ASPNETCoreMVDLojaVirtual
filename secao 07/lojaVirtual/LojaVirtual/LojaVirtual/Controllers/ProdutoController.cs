9026cb86da17ca83d17cf7b3343cb255f4025372
samuellemes1 <46980821+samuellemes1@users.noreply.github.com>
2019-11-18 00:06:38 -0300

    alteração de rota

---
+++ secao 07/lojaVirtual/LojaVirtual/LojaVirtual/Controllers/ProdutoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Controllers
+{
+    public class ProdutoController
+    {
+        public string visualizar()
+        {
+            return "Produto -> visualizar";
+
+        }
+
+
+    }
+}
